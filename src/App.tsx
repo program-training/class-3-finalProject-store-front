28e0572290ee302d08198ecb0372b0811d2c2ef2
shuli01~ <shuli7000@gmail.com>
2023-11-20 16:08:32 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp"
+import ResponsiveAppBar from "./components/Header/Header";
 
 function App() {
   return (
     <>
+    <ResponsiveAppBar/>
       <BrowserRouter>
         <Routes>
+          
           <Route path="/signUp" element={<SignUp />}>
           </Route>
         </Routes>

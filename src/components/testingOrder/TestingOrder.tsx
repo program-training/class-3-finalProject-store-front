c5de5baf3f1ad853d1db90af1d2b37058457c9db
ari shtipi <arish186777@gmail.com>
2023-11-22 12:01:20 +0200

    testing ORDER to OSM

---
+++ src/components/testingOrder/TestingOrder.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import axios from "axios";
+
+export default function SendOrders() {
+  const sendOrderToServer = async () => {
+    try {
+      const sampleOrder = {
+        id: "123456",
+        cartItems: [
+          {
+            id: "product1",
+            name: "Product 1",
+            description: "Description for Product 1",
+            price: 20,
+            quantity: 2,
+          },
+          {
+            id: "product2",
+            name: "Product 2",
+            description: "Description for Product 2",
+            price: 30,
+            quantity: 1,
+          },
+        ],
+        orderTime: "2023-11-22T12:00:00",
+        status: "Pending",
+        price: 70,
+        shippingDetails: {
+          address: "123 Main St",
+          userId: 1,
+          contactNumber: "555-1234",
+          orderType: "Express",
+        },
+      };
+
+      const response = await axios.post(
+        `http://localhost:5050/orders/order`,
+        sampleOrder
+      );
+      console.log("Server response:", response.data);
+    } catch (error) {
+      console.error("Error sending order:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={sendOrderToServer}>Send Order</button>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import SendOrders from './components/testingOrder/TestingOrder.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SendOrders/>
   </React.StrictMode>,
 )

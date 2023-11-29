5bdc92eba8ed499a6b722e9ac098777b48316fbc
unknown <arish186777@gmail.com>
2023-11-29 10:07:16 +0200

    func to redux & castyum hooh

--- src/hooks/CartReduxHook.ts
+++ src/hooks/CartReduxHook.ts
@@ -2,33 +2,34 @@ import axios, { Method } from "axios";
 import { useAppDispatch } from "../redux/hooks";
 import { upDateUserCart } from "../redux/userCartSlice";
 
-const UserCartRedux = (method: Method) => {
+const UserCartRedux = (method: Method, search?: string) => {
   const dispatch = useAppDispatch();
-
   const getToken = localStorage.getItem("token");
+
   if (!getToken) {
     throw new Error("Token is not found");
   }
-  const token = JSON.parse(getToken);
 
   const fetchCart = async () => {
     try {
       const response = await axios({
         method,
-        url: `${import.meta.env.VITE_BASE_URL}/api/carts`,
+        url: `${import.meta.env.VITE_BASE_URL}/api/carts${search ? `/${search}` : ""}`,
         headers: {
-          Authorization: `${token}`,
+          Authorization: `${getToken}`,
         },
       });
 
-      const cartData = response.data;
-      dispatch(upDateUserCart(cartData));
-      console.log(cartData);
+      if (response.statusText === "OK") {
+        const cartData = response.data;
+        dispatch(upDateUserCart(cartData));
+      } else {
+        throw new Error("Response is NOT OK");
+      }
     } catch (err) {
       console.error(err);
     }
   };
-
   return fetchCart;
 };
 

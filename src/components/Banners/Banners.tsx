d6fb2087f643d674ffe32d34b45354b47563f245
moishy friedman <moshehlf@gmail.com>
2023-11-30 14:04:56 +0200

    create banner component

--- src/components/Banners/Banners.tsx
+++ src/components/Banners/Banners.tsx
@@ -1,25 +1,5 @@
-import axios from "axios";
 import "./banner.css";
-import { useEffect, useState } from "react";
 
 export default function Banners(prop: { categoryName: string }) {
-  const [banner, setBanner] = useState<string>("");
-  useEffect(() => {
-    async function getBanner() {
-      try {
-        const result = await axios.get(`${import.meta.env.VITE_BASE_BANNERS}/banner/api/getBanner/?categoryName=${prop.categoryName}&location=top`);
-        if (result.statusText !== "OK") {
-          throw result;
-        }
-        setBanner(result.data);
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          console.log(error);
-        }
-      }
-    }
-    getBanner();
-  }, []);
-
-  return <iframe id="banner" src={banner} />;
+  return <iframe id="banner" src={`${import.meta.env.VITE_BASE_BANNERS}/banner/api/getBanner/?categoryName=${prop.categoryName}&location=top`}></iframe>;
 }

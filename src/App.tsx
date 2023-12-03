ecd9c53647c3fc35dc0cdfab3c9c6406f1dbbeb0
moishy friedman <moshehlf@gmail.com>
2023-12-03 13:42:40 +0200

    change compoonent name to productsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LayersHeader from "./components/Header/Layers";
-import ProductPage from "./components/ProductPage/ProductPage";
+import ProductPage from "./components/ProductPage/ProductsPage";
 import Home from "./components/Home/Home";
 import CartPage from "./components/CartPage/CartPage";
 

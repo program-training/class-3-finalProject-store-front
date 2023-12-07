d95ca3d391ab6f1ecde8a1183d6fd7bd75ed9ffb
unknown <arish186777@gmail.com>
2023-12-07 13:19:20 +0200

    Creating a data graph of website visits &  Design the graph

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -44,14 +44,17 @@ export default function Graph() {
   }, [data]);
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
+<Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        Display usage data
       <Box sx={{ width: "70%" }}>
         {graphData && (
-          <VictoryChart width={650} height={300} domainPadding={{ x: 13 }}>
-            <VictoryAxis tickValues={Object.keys(graphData)} />
-            <VictoryAxis dependentAxis tickFormat={(x) => x} style={{ tickLabels: { fill: "#8569" } }} />
-            <VictoryBar data={Object.entries(graphData).map(([hour, quantity]) => ({ hour, quantity }))} x="hour" y="quantity" style={{ data: { fill: "red" } }} />
-          </VictoryChart>
+          <>
+            <VictoryChart width={650} height={300} domainPadding={{ x: 13 }}>
+              <VictoryAxis tickValues={Object.keys(graphData)} />
+              <VictoryAxis dependentAxis tickFormat={(x) => x} style={{ tickLabels: { fill: "#8569" } }} />
+              <VictoryBar data={Object.entries(graphData).map(([hour, quantity]) => ({ hour, quantity }))} x="hour" y="quantity" style={{ data: { fill: "red" } }} />
+            </VictoryChart>
+          </>
         )}
       </Box>
     </Box>
